5955691734388473a1f49506b2a2f30d684c26df
Renan Stefani <renanstefanir@gmail.com>
2022-12-25 20:49:55 -0300

    Inserindo verificação de método de requisição
    Inserimos a condição para que o script seja executado apenas quando o método for o ideal

--- APIListarEstados.php
+++ APIListarEstados.php
@@ -1,55 +1,58 @@
 <?php
-//definimos a compatibilidade apontando o tipo de conteúdo:
-header('Content-type: application/json');
+// Verificamos o tipo de requisição para darmos continuidade
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    //definimos a compatibilidade apontando o tipo de conteúdo:
+    header('Content-type: application/json');
 
-// Conexão com o DB
-require_once('dbConnect.php');
+    // Conexão com o DB
+    require_once('dbConnect.php');
 
-// Definindo UTF8 e evitando conflitos
-mysqli_set_charset($conn, $charset);
+    // Definindo UTF8 e evitando conflitos
+    mysqli_set_charset($conn, $charset);
 
-//criamos um array vazio para receber o select
-$response = array();
+    //criamos um array vazio para receber o select
+    $response = array();
 
-//realizando a consulta
-$stmt = mysqli_prepare($conn, 'SELECT id, sigla, nome FROM estado');
+    //realizando a consulta
+    $stmt = mysqli_prepare($conn, 'SELECT id, sigla, nome FROM estado');
 
-//executando o select
-mysqli_stmt_execute($stmt);
+    //executando o select
+    mysqli_stmt_execute($stmt);
 
-//armazenando a resposta
-mysqli_stmt_store_result($stmt);
+    //armazenando a resposta
+    mysqli_stmt_store_result($stmt);
 
-//definindo os dados recebidos
-mysqli_stmt_bind_result(
-    $stmt,
-    $id,
-    $sigla,
-    $nome
-);
+    //definindo os dados recebidos
+    mysqli_stmt_bind_result(
+        $stmt,
+        $id,
+        $sigla,
+        $nome
+    );
 
-// var_dump($stmt);
+    // var_dump($stmt);
 
-// se o numero de linhas for maior que zero
-// significa que possui dados
-if(mysqli_stmt_num_rows($stmt) > 0){
+    // se o numero de linhas for maior que zero possui dado a ser retornado
+    if (mysqli_stmt_num_rows($stmt) > 0) {
 
-    while(mysqli_stmt_fetch($stmt)){
-        
-        array_push($response, array(
-            "id" => $id,
-            "sigla" => $sigla,
-            "nome" => $nome
-            )
-        );
+        while (mysqli_stmt_fetch($stmt)) {
+
+            array_push(
+                $response,
+                array(
+                    "id" => $id,
+                    "sigla" => $sigla,
+                    "nome" => $nome
+                )
+            );
+        }
+
+        echo json_encode($response);
+
+    } else {
+        echo json_encode($response);
     }
 
-    echo json_encode($response);
-
-}else{
-    echo json_encode($response);
 }
-
-
 ?>
